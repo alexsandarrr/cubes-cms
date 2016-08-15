2dee287466e9eeb9f8b27e7e59a0d61771d081ca
Aleksandar Stevanovic <alexsandarrr@gmail.com>
2016-08-15 20:09:39 +0200

    15.08.2016.

---
+++ application/controllers/Admin/IndexslidesController.php
@@ -0,0 +1,499 @@
+<?php
+
+class Admin_IndexSlidesController extends Zend_Controller_Action
+{
+    public function indexAction () {
+        //prikaz svih indexSlide-a
+        $flashMessenger = $this->getHelper('FlashMessenger');
+        
+        $systemMessages = array(
+            'success' => $flashMessenger->getMessages('success'),
+            'errors' => $flashMessenger->getMessages('errors'),
+        );
+        
+        $cmsIndexSlidesDbTable = new Application_Model_DbTable_CmsIndexSlides();
+        
+        $indexSlides = $cmsIndexSlidesDbTable->search(array(
+//            'filters' => array(
+//                'id' => array(1, 3, 5, 7)
+//            ),
+            'orders' => array(
+                'order_number' => 'ASC'
+            ),
+            //'limit' => 4,
+            //'page' => 3
+        ));
+        
+        $this->view->indexSlides = $indexSlides;
+        $this->view->systemMessages = $systemMessages;
+    }
+    
+    public function addAction () {
+        
+        $request = $this->getRequest();
+        
+        $flashMessenger = $this->getHelper('FlashMessenger');
+
+        $form = new Application_Form_Admin_IndexSlideAdd();
+
+        //default form data
+        $form->populate(array(
+            
+        ));
+
+        $systemMessages = array(
+            'success' => $flashMessenger->getMessages('success'),
+            'errors' => $flashMessenger->getMessages('errors'),
+        );
+
+        if ($request->isPost() && $request->getPost('task') === 'save') {
+
+            try {
+
+                //check form is valid
+                if (!$form->isValid($request->getPost())) {
+                    throw new Application_Model_Exception_InvalidInput('Invalid data was sent for new indexSlide');
+                }
+
+                //get form data
+                $formData = $form->getValues();
+                
+                // remove key index_slide_photo from form data because there is no column 'index_slide_photo' in cms_indexSlides table
+                unset($formData['index_slide_photo']);
+                
+                $cmsIndexSlidesTable = new Application_Model_DbTable_CmsIndexSlides();
+                
+                // insert indexSlide returns ID of the new indexSlide
+                $indexSlideId = $cmsIndexSlidesTable->insertIndexSlide($formData);
+                
+                if ($form->getElement('index_slide_photo')->isUploaded()) {
+                    // photo is uploaded
+                    
+                    $fileInfos = $form->getElement('index_slide_photo')->getFileInfo('index_slide_photo');
+                    $fileInfo = $fileInfos['index_slide_photo'];
+                    
+                    try {
+                        // open uploaded photo in temporary directory
+                        $indexSlidePhoto = Intervention\Image\ImageManagerStatic::make($fileInfo['tmp_name']);
+                        
+                        $indexSlidePhoto->fit(600, 400);
+                        
+                        $indexSlidePhoto->save(PUBLIC_PATH . '/uploads/index-slides/' . $indexSlideId . '.jpg');
+                        
+                    } catch (Exception $ex) {
+                        
+                        $flashMessenger->addMessage('IndexSlide has been saved but error occured during image processing', 'errors');
+
+                        //redirect to same or another page
+                        $redirector = $this->getHelper('Redirector');
+                        $redirector->setExit(true)
+                                ->gotoRoute(array(
+                                    'controller' => 'admin_indexslides',
+                                    'action' => 'edit',
+                                    'id' => $indexSlideId
+                                        ), 'default', true);
+                    }
+                    
+                    //$fileInfo = $_FILES['index_slide_photo']; moze i ovako
+                }
+
+                // do actual task
+                //save to database etc
+                
+                
+                
+                //set system message
+                $flashMessenger->addMessage('IndexSlide has been saved', 'success');
+
+                //redirect to same or another page
+                $redirector = $this->getHelper('Redirector');
+                $redirector->setExit(true)
+                        ->gotoRoute(array(
+                            'controller' => 'admin_indexslides',
+                            'action' => 'index'
+                                ), 'default', true);
+            } catch (Application_Model_Exception_InvalidInput $ex) {
+                $systemMessages['errors'][] = $ex->getMessage();
+            }
+        }
+
+        $this->view->systemMessages = $systemMessages;
+        $this->view->form = $form;
+    }
+    
+    public function editAction () {
+        
+        $request = $this->getRequest();
+        
+        $id = (int) $request->getParam('id');
+        
+        if ($id <= 0) {
+            
+            // prekida se izvrsavanje programa i prikazuje se "Page not found"
+            throw new Zend_Controller_Router_Exception('Invalid indexSlide id: ' . $id, 404);
+        }
+        
+        $cmsIndexSlidesTable = new Application_Model_DbTable_CmsIndexSlides();
+                
+        $indexSlide = $cmsIndexSlidesTable->getIndexSlideById($id);
+        
+        if (empty($indexSlide)) {
+            throw new Zend_Controller_Router_Exception('No indexSlide is found with id: ' . $id, 404);
+        }
+        
+        
+        
+        $flashMessenger = $this->getHelper('FlashMessenger');
+
+        $form = new Application_Form_Admin_IndexSlideAdd();
+
+        //default form data
+        $form->populate($indexSlide);
+
+        $systemMessages = array(
+            'success' => $flashMessenger->getMessages('success'),
+            'errors' => $flashMessenger->getMessages('errors'),
+        );
+
+        if ($request->isPost() && $request->getPost('task') === 'update') {
+
+            try {
+
+                // check form is valid
+                if (!$form->isValid($request->getPost())) {
+                    throw new Application_Model_Exception_InvalidInput('Invalid data was sent for indexSlide');
+                }
+
+                // get form data
+                $formData = $form->getValues();
+                
+                unset($formData['index_slide_photo']);
+                
+                if ($form->getElement('index_slide_photo')->isUploaded()) {
+                    // photo is uploaded
+                    
+                    $fileInfos = $form->getElement('index_slide_photo')->getFileInfo('index_slide_photo');
+                    $fileInfo = $fileInfos['index_slide_photo'];
+                    
+                    try {
+                        // open uploaded photo in temporary directory
+                        $indexSlidePhoto = Intervention\Image\ImageManagerStatic::make($fileInfo['tmp_name']);
+                        
+                        $indexSlidePhoto->fit(600, 400);
+                        
+                        $indexSlidePhoto->save(PUBLIC_PATH . '/uploads/index-slides/' . $indexSlide['id'] . '.jpg');
+                        
+                    } catch (Exception $ex) {
+                        
+                        throw new Application_Model_Exception_InvalidInput('Error occured during image processing');
+                        
+                    }
+                    
+                    //$fileInfo = $_FILES['index_slide_photo']; moze i ovako
+                }
+                
+                // radimo update postojeceg zapisa u tabeli
+                $cmsIndexSlidesTable->updateIndexSlide($indexSlide['id'], $formData);
+
+                // do actual task
+                // save to database etc
+                
+                // set system message
+                $flashMessenger->addMessage('IndexSlide has been updated', 'success');
+
+                // redirect to same or another page
+                $redirector = $this->getHelper('Redirector');
+                $redirector->setExit(true)
+                        ->gotoRoute(array(
+                            'controller' => 'admin_indexslides',
+                            'action' => 'index'
+                                ), 'default', true);
+            } catch (Application_Model_Exception_InvalidInput $ex) {
+                $systemMessages['errors'][] = $ex->getMessage();
+            }
+        }
+
+        $this->view->systemMessages = $systemMessages;
+        $this->view->form = $form;
+        
+        $this->view->indexSlide = $indexSlide;
+    }
+    
+    public function deleteAction () {
+        
+        $request = $this->getRequest();
+        
+        if (!$request->isPost() || $request->getPost('task') != 'delete') {
+            // request is not post
+            // or task is not delete
+            // redirect to index page
+            
+            $redirector = $this->getHelper('Redirector');
+                $redirector->setExit(true)
+                        ->gotoRoute(array(
+                            'controller' => 'admin_indexslides',
+                            'action' => 'index'
+                                ), 'default', true);
+        }
+        
+        $flashMessenger = $this->getHelper('FlashMessenger');
+        
+        try {
+            // read $_POST['id]
+        $id = (int) $request->getPost('id');
+        
+        if ($id <= 0) {
+            
+            throw new Application_Model_Exception_InvalidInput('Invalid indexSlide id: ' . $id);
+            
+        }
+        
+        $cmsIndexSlidesTable = new Application_Model_DbTable_CmsIndexSlides();
+                
+        $indexSlide = $cmsIndexSlidesTable->getIndexSlideById($id);
+        
+        if (empty($indexSlide)) {
+            
+            throw new Application_Model_Exception_InvalidInput('No indexSlide is found with id: ' . $id);
+            
+        }
+        
+        $cmsIndexSlidesTable->deleteIndexSlide($id);
+        
+        $flashMessenger->addMessage('IndexSlide ' . $indexSlide['title'] . ' has been deleted', 'success');
+            
+            $redirector = $this->getHelper('Redirector');
+                $redirector->setExit(true)
+                        ->gotoRoute(array(
+                            'controller' => 'admin_indexslides',
+                            'action' => 'index'
+                                ), 'default', true);
+        
+    
+        } catch (Application_Model_Exception_InvalidInput $ex) {
+            
+            $flashMessenger->addMessage($ex->getMessage(), 'errors');
+            
+            $redirector = $this->getHelper('Redirector');
+                $redirector->setExit(true)
+                        ->gotoRoute(array(
+                            'controller' => 'admin_indexslides',
+                            'action' => 'index'
+                                ), 'default', true);
+        }
+        
+    }
+    
+    public function disableAction () {
+        
+        $request = $this->getRequest();
+        
+        if (!$request->isPost() || $request->getPost('task') != 'disable') {
+            // request is not post
+            // or task is not delete
+            // redirect to index page
+            
+            $redirector = $this->getHelper('Redirector');
+                $redirector->setExit(true)
+                        ->gotoRoute(array(
+                            'controller' => 'admin_indexslides',
+                            'action' => 'index'
+                                ), 'default', true);
+        }
+        
+        $flashMessenger = $this->getHelper('FlashMessenger');
+        
+        try {
+            // read $_POST['id]
+        $id = (int) $request->getPost('id');
+        
+        if ($id <= 0) {
+            
+            throw new Application_Model_Exception_InvalidInput('Invalid indexSlide id: ' . $id);
+            
+        }
+        
+        $cmsIndexSlidesTable = new Application_Model_DbTable_CmsIndexSlides();
+                
+        $indexSlide = $cmsIndexSlidesTable->getIndexSlideById($id);
+        
+        if (empty($indexSlide)) {
+            
+            throw new Application_Model_Exception_InvalidInput('No indexSlide is found with id: ' . $id);
+            
+        }
+        
+        $cmsIndexSlidesTable->disableIndexSlide($id);
+        
+        $flashMessenger->addMessage('IndexSlide ' . $indexSlide['title'] . ' has been disabled', 'success');
+            
+            $redirector = $this->getHelper('Redirector');
+                $redirector->setExit(true)
+                        ->gotoRoute(array(
+                            'controller' => 'admin_indexslides',
+                            'action' => 'index'
+                                ), 'default', true);
+        
+    
+        } catch (Application_Model_Exception_InvalidInput $ex) {
+            
+            $flashMessenger->addMessage($ex->getMessage(), 'errors');
+            
+            $redirector = $this->getHelper('Redirector');
+                $redirector->setExit(true)
+                        ->gotoRoute(array(
+                            'controller' => 'admin_indexslides',
+                            'action' => 'index'
+                                ), 'default', true);
+        }
+        
+    }
+
+    public function enableAction () {
+        
+        $request = $this->getRequest();
+        
+        if (!$request->isPost() || $request->getPost('task') != 'enable') {
+            // request is not post
+            // or task is not delete
+            // redirect to index page
+            
+            $redirector = $this->getHelper('Redirector');
+                $redirector->setExit(true)
+                        ->gotoRoute(array(
+                            'controller' => 'admin_indexslides',
+                            'action' => 'index'
+                                ), 'default', true);
+        }
+        
+        $flashMessenger = $this->getHelper('FlashMessenger');
+        
+        try {
+            // read $_POST['id]
+        $id = (int) $request->getPost('id');
+        
+        if ($id <= 0) {
+            
+            throw new Application_Model_Exception_InvalidInput('Invalid indexSlide id: ' . $id);
+            
+        }
+        
+        $cmsIndexSlidesTable = new Application_Model_DbTable_CmsIndexSlides();
+                
+        $indexSlide = $cmsIndexSlidesTable->getIndexSlideById($id);
+        
+        if (empty($indexSlide)) {
+            
+            throw new Application_Model_Exception_InvalidInput('No indexSlide is found with id: ' . $id);
+            
+        }
+        
+        $cmsIndexSlidesTable->enableIndexSlide($id);
+        
+        $flashMessenger->addMessage('IndexSlide ' . $indexSlide['title'] . ' has been enabled', 'success');
+            
+            $redirector = $this->getHelper('Redirector');
+                $redirector->setExit(true)
+                        ->gotoRoute(array(
+                            'controller' => 'admin_indexslides',
+                            'action' => 'index'
+                                ), 'default', true);
+        
+    
+        } catch (Application_Model_Exception_InvalidInput $ex) {
+            
+            $flashMessenger->addMessage($ex->getMessage(), 'errors');
+            
+            $redirector = $this->getHelper('Redirector');
+                $redirector->setExit(true)
+                        ->gotoRoute(array(
+                            'controller' => 'admin_indexslides',
+                            'action' => 'index'
+                                ), 'default', true);
+        }
+        
+    }
+    
+    public function updateorderAction () {
+        
+        $request = $this->getRequest();
+        
+        if (!$request->isPost() || $request->getPost('task') != 'saveOrder') {
+            // request is not post
+            // or task is not saveOrder
+            // redirect to index page
+            
+            $redirector = $this->getHelper('Redirector');
+                $redirector->setExit(true)
+                        ->gotoRoute(array(
+                            'controller' => 'admin_indexslides',
+                            'action' => 'index'
+                                ), 'default', true);
+        }
+        
+        $flashMessenger = $this->getHelper('FlashMessenger');
+        
+        try {
+            
+            $sortedIds = $request->getPost('sorted_ids');
+            
+            if(empty($sortedIds)) {
+                throw new Application_Model_Exception_InvalidInput('Sortered ids are not sent');
+            }
+            
+            $sortedIds = trim($sortedIds, ' ,');
+            
+            if (!preg_match('/^[0-9]+(,[0-9]+)*$/', $sortedIds)) {
+                throw new Application_Model_Exception_InvalidInput('Invalid sorted ids: ' . $sortedIds);
+            }
+            
+            $sortedIds = explode(',', $sortedIds);
+            
+            $cmsIndexSlidesTable = new Application_Model_DbTable_CmsIndexSlides();
+            
+            $cmsIndexSlidesTable->updateOrderOfIndexSlides($sortedIds);
+            
+            $flashMessenger->addMessage('Order is successfully saved', 'success');
+            
+            $redirector = $this->getHelper('Redirector');
+                $redirector->setExit(true)
+                        ->gotoRoute(array(
+                            'controller' => 'admin_indexslides',
+                            'action' => 'index'
+                                ), 'default', true);
+            
+        } catch (Application_Model_Exception_InvalidInput $ex) {
+            
+            $flashMessenger->addMessage($ex->getMessage(), 'errors');
+            
+            $redirector = $this->getHelper('Redirector');
+                $redirector->setExit(true)
+                        ->gotoRoute(array(
+                            'controller' => 'admin_indexslides',
+                            'action' => 'index'
+                                ), 'default', true);
+        
+        }
+        
+    }
+    
+    public function dashboardAction () {
+        
+        $cmsIndexSlidesDbTable = new Application_Model_DbTable_CmsIndexSlides();
+        
+        $indexSlidesTotal = $cmsIndexSlidesDbTable->count(array(
+            'orders' => array(
+                'order_number' => 'ASC'
+                )
+        ));
+        
+        $indexSlidesActive = $cmsIndexSlidesDbTable->count(array(
+            'status' => Application_Model_DbTable_CmsIndexSlides::STATUS_ENABLED
+        ));
+        
+        $this->view->indexSlidesTotal = $indexSlidesTotal;
+        $this->view->indexSlidesActive = $indexSlidesActive;
+        
+        }
+}
+
---
+++ application/views/helpers/IndexSlideImgUrl.php
@@ -0,0 +1,23 @@
+<?php
+
+class Zend_View_Helper_IndexSlideImgUrl extends Zend_View_Helper_Abstract
+{
+    public function indexSlideImgUrl($indexSlide) {
+        
+        $indexSlideImgFileName = $indexSlide['id'] . '.jpg';
+        
+        // APPLICATION_PATH putanja do aplikacije (folder application)
+        $indexSlideImgFilePath = PUBLIC_PATH . "/uploads/index-slides/" . $indexSlideImgFileName;
+        
+        //Helper ima property view koji je Zend_View
+        //i preko kojeg pozivamo ostale view helpere
+        //na primer $this->view->baseUrl()
+        
+        if (is_file($indexSlideImgFilePath)) {
+            return $this->view->baseUrl('/uploads/index-slides/' . $indexSlideImgFileName);
+        } else {
+            return '';
+        }
+    }
+}
+
